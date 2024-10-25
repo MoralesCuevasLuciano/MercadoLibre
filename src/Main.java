bbe5def01b7c6e49efda6be7f86113df8943e779
Richard <richardmorenoar@gmail.com>
2024-10-25 18:35:31 -0300

    Se agregaron clases para la compra del carrito

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import enums.*;
 import enums.UsoCalzadoPantalon;
 import models.ArrayList.AdministradorList;
+import models.ArrayList.CartList;
+import models.ArrayList.CartMap;
 import models.Hogar.Bazar;
 import models.Hogar.Escritorio;
 import models.Hogar.Mueble;
@@ -9,7 +11,6 @@ import models.Ropa.Calzado;
 import models.ArrayList.CarritoList;
 import models.Usuario.Cliente;
 import models.Usuario.Direccion;
-import models.Usuario.ItemCarrito;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,30 +18,37 @@ public class Main {
 
 
         Bazar a = new Bazar("Bazar Multiusos", "MarcaX", "ModeloY", 49.99, 20, 1.5f, CategoriasHogar.JARDIN, UsoHogar.COMER, "Plástico", true, TamañoBazar.MEDIANO);
-
-        // Crear un objeto Mueble
         Mueble i = new Mueble("Silla de Comedor", "MarcaA", "ModeloB", 1, 50, 3.0f, CategoriasHogar.JARDIN, UsoHogar.COMER, "Madera", 4, EstiloMueble.LIVING);
-
-        // Crear un objeto Escritorio
         Escritorio o = new Escritorio("Escritorio Ejecutivo", "MarcaC", "ModeloD", 1, 10, 30.0f, CategoriasHogar.JARDIN, UsoHogar.DECORACION, "Madera", 2, EstiloMueble.LIVING, 3, true);
-
         Direccion q = new Direccion();
         CarritoList t = new CarritoList();
         Sillon e = new Sillon();
         AdministradorList admin = new AdministradorList();
-
         Calzado zapatillas = new Calzado("Zapatillas Deportivas", "Nike", "Air Max", 1, 20, "Negro", Genero.UNISEX, TalleRemera.M, "Sintético", TipoCalzado.BORCEGO, UsoCalzadoPantalon.CASUAL);
-
         Cliente cliente = new Cliente("C001", "Ana Gómez", "98765432B", Genero.FEMENINO, "555-9876", q, t, 40150.75);
+        cliente.imprimir();
+        CartList p = new CartList();
+        CartMap d = new CartMap();
 
+        d.agregarAlCarrito(o,4);
+        d.agregarAlCarrito(i,4);
+
+        d.mostrarCarrito();
+
+        d.comprarCarrito(cliente);
         cliente.imprimir();
 
-        /**Agregar los objetos al repertorio de ventas*/
+        o.imprimir();
+        i.imprimir();
+
+
+
+        /**Agregar los objetos al repertorio de ventas
         admin.add(zapatillas);
         admin.edit(zapatillas, "zapatinas", "adidas", "modelo entrecasa", 20000, 5);
         zapatillas.imprimir();
 
-        /** Agregar objeto al carrito*/
+        /** Agregar objeto al carrito
 
         ItemCarrito i1 = new ItemCarrito(zapatillas, 2); //crea un item de 2 zapatillas
 
@@ -49,7 +57,7 @@ public class Main {
         System.out.println("El stock es " + zapatillas.getStock());
         System.out.println(t.comprarCarrito1(cliente)); //se compran 2 zapatillas si hay saldo suf
         System.out.println("El stock es " + zapatillas.getStock());
-        cliente.getCarrito().mostrarCarrito();
+        cliente.getCarrito().mostrarCarrito();*/
 
     }
 }
---
+++ src/models/ArrayList/CartList.java
@@ -0,0 +1,94 @@
+package models.ArrayList;
+
+import models.Producto;
+import models.Usuario.Cliente;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartList {
+    private List<Producto> carrito = new ArrayList<>();
+    private List<Integer> cantidad = new ArrayList<>();
+
+    /**CONSTRUCTORES*/
+
+    public CartList() {
+        this.carrito = new ArrayList<>();
+        this.cantidad = new ArrayList<>();
+    }
+
+    /**METODOS: FALTAN VALIDACIONES PERSO*/
+
+    /**AGREGA AL CARRITO*/
+    public void addToCart(Producto p, int cantidad){
+        if(p.getStock()>=cantidad){
+            this.carrito.add(p);
+            this.cantidad.add(cantidad);
+        }else{
+            System.out.println("No hay disponible la cantidad que deseas " + "Solo hay " + p.getStock());
+        }
+    }
+    /**ELIMINA EL CARRITO*/
+    public void eliminarTodoDelCarrito(){
+     this.carrito.clear();
+     this.cantidad.clear();
+    }
+
+    /**COMPRA EL CARRITO*/
+
+    public void comprarCart(Cliente c){
+        //CALCULA EL PRECIO TOTAL DEL CARRITO Y LO RETONA
+        double total = calucarPrecioTotal();
+
+        // VALIDAMOS QUE TENGA UN MANGO PRIMERO
+        if(c.getSaldo()>=total){
+            for(int i=0; i<this.carrito.size();i++){
+                //COMO NATURALMENTE LAS LISTAS TENDRAN EL MISMO TAMAÑO, SE USA UN FOR PARA SACAR SU INFO
+                Producto p = this.carrito.get(i); //RECORREMOS PRODUCTO I
+                int cantidades = this.cantidad.get(i); //RECORREMOS LA CANTIDAD DELE PRODUCTO I
+
+                //ACTUALIZAMOS EL STOCK
+                p.stock((p.getStock()-cantidades));
+        }
+            //RESTAMOS LOS MANGOS
+            c.saldo(c.getSaldo()-total);
+            eliminarTodoDelCarrito();
+            System.out.println("Compra realizada con exito");
+        }else{
+            System.out.println("No es posible comprar el carrito");
+        }
+    }
+
+    public void mostrarCarrito() {
+        double total = calucarPrecioTotal();
+        if (carrito.isEmpty()) {
+            System.out.println("El carrito está vacío.");
+        } else {
+            System.out.println("Contenido del carrito:");
+            for (int i = 0; i < carrito.size(); i++) {
+                Producto p = carrito.get(i);
+                int cantidadProducto = cantidad.get(i);
+                System.out.println("Producto: " + p.getNombre() +
+                        ", Precio: $" + p.getPrecio() +
+                        ", Cantidad: " + cantidadProducto +
+                        ", Subtotal: $" + (p.getPrecio() * cantidadProducto));
+            }
+            System.out.println("Total a pagar: $" + total);
+        }
+    }
+
+    /**CUANTO VALE EL CARRITO*/
+
+    public double calucarPrecioTotal(){
+        double total = 0;
+        for(int i=0; i<this.carrito.size();i++){
+            Producto p = this.carrito.get(i);
+            int cantidades = this.cantidad.get(i);
+
+            total= total+ (p.getPrecio() * cantidades);
+        }
+        return total;
+    }
+}
+
+
