a6bd4f80f2699af13666d90bfce216bc277cbeae
PGOviedo94 <pablogermanoviedo@gmail.com>
2024-10-28 21:11:45 -0300

    cree la clase productosJson e importe las dependencias de json simple

---
+++ src/models/ProductosJSON/ProductosJSON.java
@@ -0,0 +1,23 @@
+package models.ProductosJSON;
+
+import models.Producto;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductosJSON {
+
+    private static final String PATH_RESOURCES = System.getProperty("user.dir") + "/src/resources";
+    private static final String PATH_PRODUCTO = "/producto.json";
+
+    private final List <Producto> productsArray = new ArrayList (1000);
+
+
+
+
+
+
+}
